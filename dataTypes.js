e52e91d7fa3bf2ba772c8111f199ac5d3524ee11
Sumit Kumar <42406053+sumitkumar0608@users.noreply.github.com>
2026-02-13 10:01:14 +0000

    dataTypes

---
+++ dataTypes.js
@@ -0,0 +1,15 @@
+//String
+let name = "Sumit"
+//number
+let age = 26;
+//Boolean
+let isActive = true
+//Undefined
+let isNull = null;
+
+//null
+let val;
+
+//console.table([name,age,isActive])
+console.log(typeof(val));
+console.log(typeof(isNull));
