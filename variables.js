212db10e08d6273c9357fc8e57467c8dbd7ca80d
Sumit Kumar <42406053+sumitkumar0608@users.noreply.github.com>
2026-02-13 09:55:37 +0000

    variables

---
+++ variables.js
@@ -0,0 +1,9 @@
+const accountID = "SumiK06";
+//const values can't be changed.
+//accountID = "14456"
+
+let accountName = "Sumit Kumar";
+accountName ="Test Sumit Kumar"
+
+console.log(accountID)
+console.log(accountName);
