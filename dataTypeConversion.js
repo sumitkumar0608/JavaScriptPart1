490c185a0e1978eaadfb338712cfe887a1d5a125
Sumit Kumar <42406053+sumitkumar0608@users.noreply.github.com>
2026-02-13 10:11:01 +0000

    operations & conversions

---
+++ dataTypeConversion.js
@@ -0,0 +1,14 @@
+let num = 33;
+console.log(typeof num);
+
+let val = "35A";
+let valNumber = Number(val);
+
+console.log(valNumber)
+console.log(typeof valNumber);
+
+let bolVal = 1;
+let bolVal2 = "";
+console.log(Boolean(bolVal)); //true
+console.log(Boolean(bolVal2)); //false
+
---
+++ operations.js
@@ -0,0 +1,10 @@
+let val = 3;
+let negVal = -val
+
+//console.log(negVal);
+
+let str1 = "Sumit"
+let str2 = " Kumar"
+let str3 = str1 + str2
+str3 = str1 - str2
+console.log(str3)
