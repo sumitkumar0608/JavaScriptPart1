20174e777bc5d38f6b1c4f57f2da92673fea14dd
Sumit Kumar <42406053+sumitkumar0608@users.noreply.github.com>
2026-02-13 10:23:15 +0000

    datatypes primitive and non primitve

--- dataTypes.js
+++ dataTypes.js
@@ -11,5 +11,35 @@ let isNull = null;
 let val;
 
 //console.table([name,age,isActive])
-console.log(typeof(val));
-console.log(typeof(isNull));
+//console.log(typeof(val));
+//console.log(typeof(isNull));
+
+
+//Primitive DataTypes
+
+//7 Types: String, Number, Boolean, null, undefined
+
+//Reference (Non Primitive)
+
+// Array, Objects Functions
+
+
+const score = 33
+const scoreVal = 33.4
+const isChecked = true;
+let userEmail
+
+//Symbol is used to create a unique value.
+const id = Symbol(123)
+const anotherid = Symbol(123)
+
+console.log(id == anotherid);
+
+//Array is []
+//Obj is { }
+// function is (){}, it can be stored in a variable as well.
+
+const functionVal = function() {console.log("Hello from function");
+}
+functionVal
+console.log(typeof functionVal)
---
+++ operators.js
@@ -0,0 +1,5 @@
+//Strict Check Operator
+console.log("22"=== 22)
+
+//Equal Operator
+console.log("22"== 22)
