bca350ffb57781dffee192b7c2992bd6dcfcfb5d
Sumit Kumar <42406053+sumitkumar0608@users.noreply.github.com>
2026-02-13 10:53:36 +0000

    dates

---
+++ dates.js
@@ -0,0 +1,9 @@
+let myDate = new Date()
+
+let anotherDate = new Date("01-30-2026");
+
+console.log(anotherDate);
+
+let currentDate = Date.now()
+console.log(currentDate.toLocaleString('en-IN'))
+//console.log(myDate.toLocaleString('en-IN'))
