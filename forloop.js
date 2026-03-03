93146f0ae70e2be97fa6ce96f74a952ab67c5b4a
Sumit Kumar <42406053+sumitkumar0608@users.noreply.github.com>
2026-03-03 12:13:51 +0000

    forloop

--- forloop.js
+++ forloop.js
@@ -1,3 +1,6 @@
+//break is used to terminate the loop or switch statement and transfer the control to the next statement following the loop or switch.
+//continue statement is used to skip the current iteration of the loop and continue with the next iteration.
+
 for (let i = 0; i < 5; i++) {
     
     if(i == 2 || i == 1){
