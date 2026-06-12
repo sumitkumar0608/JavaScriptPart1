0732d1cb1fad5cc107c38bd6f652ad3c6d7ae5d2
Sumit Kumar <42406053+sumitkumar0608@users.noreply.github.com>
2026-06-12 09:55:56 +0000

    worked on variables

--- variables.js
+++ variables.js
@@ -1,9 +1,17 @@
 const accountID = "SumiK06";
 //const values can't be changed.
 //accountID = "14456"
+//anything defined outside any scope is global
 
 let accountName = "Sumit Kumar";
 accountName ="Test Sumit Kumar"
+//let variables can be mutated, their values can be changed
+
+var userDetails = "Test"
+var userDetails = "Another Test"
+
+//var can be redeclared, whereas let and const throws exception
+//var do leak out sometimes.
 
 console.log(accountID)
 console.log(accountName);
